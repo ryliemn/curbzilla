758ecdf480777f219dcccaf184e6be65898a9f42
Rylie Nelson <ryliemn@gmail.com>
2024-10-25 23:11:10 -0700

    before and after page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,60 +1,94 @@
+"use client";
 import ContactUs from "@/components/contact-us";
+import { BEFORE_AND_AFTERS } from "@/utils/images";
+import { Divider } from "@nextui-org/react";
+import "keen-slider/keen-slider.min.css";
 import {
-  Photo,
-  RenderImageContext,
-  RenderImageProps,
-  RowsPhotoAlbum,
-} from "react-photo-album";
+  KeenSliderInstance,
+  KeenSliderPlugin,
+  useKeenSlider,
+} from "keen-slider/react";
+import {
+  ArrowLeft,
+  CircleArrowLeft,
+  CircleArrowRight,
+  Dot,
+} from "lucide-react";
 import Image from "next/image";
-import "react-photo-album/rows.css";
-
-const photos = [
-  {
-    asset: "94993eab-247c-4ce8-bb9a-102ff5c93d00",
-    width: 1080,
-    height: 780,
-    alt: "Hiking boots",
-  },
-];
-
-function renderNextImage(
-  { alt = "", title, sizes }: RenderImageProps,
-  { photo, width, height }: RenderImageContext
-) {
-  return (
-    <div
-      style={{
-        width: "100%",
-        position: "relative",
-        aspectRatio: `${width} / ${height}`,
-      }}
-    >
-      <Image
-        fill
-        src={photo}
-        alt={alt}
-        title={title}
-        sizes={sizes}
-        placeholder={"blurDataURL" in photo ? "blur" : undefined}
-      />
-    </div>
-  );
-}
+import { MutableRefObject, useCallback, useEffect, useState } from "react";
 
 export default function Gallery() {
+  const [curIdx, setCurIdx] = useState(0);
+
+  const next = () => {
+    if (curIdx === BEFORE_AND_AFTERS.length - 1) setCurIdx(0);
+    else setCurIdx(curIdx + 1);
+  };
+
+  const prev = () => {
+    if (curIdx === 0) setCurIdx(BEFORE_AND_AFTERS.length - 1);
+    else setCurIdx(curIdx - 1);
+  };
+
   return (
-    <div className="flex-col">
-      {/* <RowsPhotoAlbum
-        photos={photos}
-        render={{ image: renderNextImage }}
-        defaultContainerWidth={1200}
-        sizes={{
-          size: "1168px",
-          sizes: [
-            { viewport: "(max-width: 1200px)", size: "calc(100vw - 32px)" },
-          ],
-        }}
-      /> */}
+    <div className="flex flex-col items-center">
+      <h1 className="text-5xl my-4">BEFORE & AFTER</h1>
+      <Divider />
+      <div className="relative">
+        <div className="my-4">
+          {BEFORE_AND_AFTERS.map((pair, idx) => {
+            return idx === curIdx ? (
+              <div key={idx} className="space-y-4">
+                <Image
+                  src={pair.before}
+                  alt="an image of curbing"
+                  width={900}
+                  height={500}
+                  className="rounded-lg"
+                />
+                <Image
+                  src={pair.after}
+                  alt="an image of curbing"
+                  width={900}
+                  height={500}
+                  className="rounded-lg"
+                />
+              </div>
+            ) : null;
+          })}
+        </div>
+        <div className="flex w-full absolute top-1/2 -translate-y-1/2">
+          <div
+            className="mr-auto bg-gray-500 rounded-full relative md:right-8"
+            onClick={prev}
+          >
+            <CircleArrowLeft
+              className="w-16 h-16 cursor-pointer"
+              color="white"
+            />
+          </div>
+          <div className="mx-auto self-center flex">
+            {BEFORE_AND_AFTERS.map((pair, idx) => {
+              return (
+                <Dot
+                  key={idx}
+                  color={idx === curIdx ? "black" : "lightgray"}
+                  className="h-5 w-5"
+                />
+              );
+            })}
+          </div>
+          <div
+            className="ml-auto bg-gray-500 rounded-full relative md:left-8"
+            onClick={next}
+          >
+            <CircleArrowRight
+              className="w-16 h-16 cursor-pointer"
+              color="white"
+            />
+          </div>
+        </div>
+      </div>
       <ContactUs />
     </div>
   );
---
+++ src/utils/images.ts
@@ -0,0 +1,26 @@
+export const BEFORE_AND_AFTERS = [
+  {
+    before: "53014d1e-fa3f-47cd-c12b-28e87558ab00",
+    after: "dd908bf9-7ad0-4383-82a4-b31f1565cd00",
+  },
+  {
+    before: "3e68de74-0092-4c9f-aeb0-ac6b06948500",
+    after: "9ab7ee2d-9a70-4c12-2964-e15419dc6500",
+  },
+  {
+    before: "3ef1fa83-e65e-4fec-c3ce-2d574a61cb00",
+    after: "8436f825-edfa-43bf-6661-3fb74bb04700",
+  },
+  {
+    before: "3d1ae231-440f-4097-59d9-a27be4e9b800",
+    after: "02fac2e9-cd1e-457f-3e10-8cc8d06fe300",
+  },
+  {
+    before: "88c91f32-8a73-42ad-7a2a-83d3545d8400",
+    after: "cf0dce9a-e3d8-43ef-db2b-8fe4799f3000",
+  },
+  {
+    before: "08975b6f-7059-48ae-0847-049d99e9ac00",
+    after: "cf73f2e5-7388-43f6-c8f2-a43c15be1b00",
+  },
+];
