0c018e9e68579bc346059a1a1c0f3864d8b5e38e
Rylie Nelson <ryliemn@gmail.com>
2024-10-27 21:46:13 -0700

    gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,23 +1,36 @@
 "use client";
+
 import ContactUs from "@/components/contact-us";
-import { BEFORE_AND_AFTERS } from "@/utils/images";
+import NextJsLightboxImage from "@/components/next-js-lightbox-image";
+import { BEFORE_AND_AFTERS, GENERIC_GALLERY } from "@/utils/images";
 import { Divider } from "@nextui-org/react";
 
 import { CircleArrowLeft, CircleArrowRight, Dot } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
+import {
+  RenderImageContext,
+  RenderImageProps,
+  RowsPhotoAlbum,
+} from "react-photo-album";
+import "react-photo-album/rows.css";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
 export default function Gallery() {
-  const [curIdx, setCurIdx] = useState(0);
+  const [curBNAIdx, setCurBNAIdx] = useState(0);
+
+  const [lightboxOpen, setLightboxOpen] = useState(false);
+  const [curLightboxIdx, setLightboxIdx] = useState(-1);
 
   const next = () => {
-    if (curIdx === BEFORE_AND_AFTERS.length - 1) setCurIdx(0);
-    else setCurIdx(curIdx + 1);
+    if (curBNAIdx === BEFORE_AND_AFTERS.length - 1) setCurBNAIdx(0);
+    else setCurBNAIdx(curBNAIdx + 1);
   };
 
   const prev = () => {
-    if (curIdx === 0) setCurIdx(BEFORE_AND_AFTERS.length - 1);
-    else setCurIdx(curIdx - 1);
+    if (curBNAIdx === 0) setCurBNAIdx(BEFORE_AND_AFTERS.length - 1);
+    else setCurBNAIdx(curBNAIdx - 1);
   };
 
   return (
@@ -27,7 +40,7 @@ export default function Gallery() {
       <div className="relative">
         <div className="my-4">
           {BEFORE_AND_AFTERS.map((pair, idx) => {
-            return idx === curIdx ? (
+            return idx === curBNAIdx ? (
               <div key={idx} className="space-y-4">
                 <Image
                   src={pair.before}
@@ -62,7 +75,7 @@ export default function Gallery() {
               return (
                 <Dot
                   key={idx}
-                  color={idx === curIdx ? "black" : "lightgray"}
+                  color={idx === curBNAIdx ? "black" : "lightgray"}
                   className="h-5 w-5"
                 />
               );
@@ -79,7 +92,46 @@ export default function Gallery() {
           </div>
         </div>
       </div>
+      <Divider className="m-4" />
+      <div className="h-auto w-full px-8 my-4">
+        <RowsPhotoAlbum
+          photos={GENERIC_GALLERY}
+          render={{ image: renderNextImage }}
+          defaultContainerWidth={1200}
+          sizes={{
+            size: "1168px",
+            sizes: [
+              { viewport: "(max-width: 1200px)", size: "calc(100vw - 32px)" },
+            ],
+          }}
+          onClick={({ index: current }) => setLightboxIdx(current)}
+        />
+      </div>
       <ContactUs />
+      <Lightbox
+        index={curLightboxIdx}
+        open={curLightboxIdx >= 0}
+        close={() => setLightboxIdx(-1)}
+        slides={GENERIC_GALLERY}
+        render={{ slide: NextJsLightboxImage }}
+      />
+    </div>
+  );
+}
+
+function renderNextImage(
+  { alt = "", title, sizes }: RenderImageProps,
+  { photo, width, height }: RenderImageContext
+) {
+  return (
+    <div
+      style={{
+        width: "100%",
+        position: "relative",
+        aspectRatio: `${width} / ${height}`,
+      }}
+    >
+      <Image fill src={photo.src} alt={alt} title={title} sizes={sizes} />
     </div>
   );
 }
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,3 +1,4 @@
+// All photos in B&A should be 16:9 aspect ratio
 export const BEFORE_AND_AFTERS = [
   {
     before: "53014d1e-fa3f-47cd-c12b-28e87558ab00",
@@ -22,5 +23,64 @@ export const BEFORE_AND_AFTERS = [
   {
     before: "08975b6f-7059-48ae-0847-049d99e9ac00",
     after: "cf73f2e5-7388-43f6-c8f2-a43c15be1b00",
+  },
+];
+
+export const GENERIC_GALLERY = [
+  {
+    src: "b9f7988e-77fc-4142-f79a-4465daa61f00",
+    name: "gallery1",
+    width: 1600,
+    height: 900,
+    alt: "Picture of curbing",
+  },
+  {
+    src: "79c52236-5823-473c-38c5-002d0ded3f00",
+    name: "gallery2",
+    width: 1600,
+    height: 900,
+    alt: "Picture of curbing",
+  },
+  {
+    src: "2b9d3c23-e652-4202-53c9-b96ba6334a00",
+    name: "gallery3",
+    width: 900,
+    height: 1600,
+    alt: "Picture of curbing",
+  },
+  {
+    src: "82836cac-1b7f-4ad8-ec37-eb570a349e00",
+    name: "gallery4",
+    width: 900,
+    height: 1600,
+    alt: "Picture of curbing",
+  },
+  {
+    src: "2d6f4b64-c925-413b-9086-be817e052800",
+    name: "gallery5",
+    width: 1600,
+    height: 900,
+    alt: "Picture of curbing",
+  },
+  {
+    src: "c4846f10-4177-49d5-6b1e-bfaace345700",
+    name: "gallery6",
+    width: 1600,
+    height: 900,
+    alt: "Picture of curbing",
+  },
+  {
+    src: "9582557a-044e-432a-74fe-99b872630300",
+    name: "gallery7",
+    width: 1600,
+    height: 900,
+    alt: "Picture of curbing",
+  },
+  {
+    src: "b33a43ca-265c-4d54-f53d-422ee9843c00",
+    name: "gallery8",
+    width: 1600,
+    height: 900,
+    alt: "Picture of curbing",
   },
 ];
